fd2bd82e4f5a032d89340da4645bd4d7cab060c7
Chi-PU <dangchi7045@gmail.com>
2026-01-19 11:19:50 +0700

    Established character class

--- Online_Star_Rail/characters.h
+++ Online_Star_Rail/characters.h
@@ -1,29 +1,59 @@
+// characters.h
 #pragma once
+#include <string>
+#include <ostream>
 
-#include <iostream>
-
-enum CharacterRarity {
-	
+enum class CharacterRarity {
+    FiveStar,
+    FourStar
 };
 
 enum class CharacterPath {
-    hunt,
-    erudition,
-    destruction,
-    harmony,
-    nihility,
-    preservation,
-    abundance,
-    remembrance,
-    elation
+    Hunt,
+    Erudition,
+    Destruction,
+    Harmony,
+    Nihility,
+    Preservation,
+    Abundance,
+    Remembrance,
+    Elation
 };
 
 enum class CharacterElement {
-    physical,
-    fire,
-    ice,
-    lightning,
-    wind,
-    quantum,
-    imaginary
+    Physical,
+    Fire,
+    Ice,
+    Lightning,
+    Wind,
+    Quantum,
+    Imaginary
+};
+
+// Helper functions for enum to string conversion
+std::string rarityToString(CharacterRarity rarity);
+std::string pathToString(CharacterPath path);
+std::string elementToString(CharacterElement element);
+
+class Character {
+public:
+    Character(std::string name, CharacterRarity rarity, CharacterPath path, CharacterElement element);
+
+    // Getters
+    const std::string& getName() const;
+    CharacterRarity getRarity() const;
+    CharacterPath getPath() const;
+    CharacterElement getElement() const;
+
+    // Display character information
+    void display() const;
+
+    // Stream operator for easy printing
+    friend std::ostream& operator<<(std::ostream& os, const Character& character);
+
+private:
+    std::string m_name;
+    CharacterRarity m_rarity;
+    CharacterPath m_path;
+    CharacterElement m_element;
 };
