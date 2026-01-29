8b4ee3614d29f22245c60f15b72866468f3296bf
Chi-PU <dangchi7045@gmail.com>
2026-01-29 10:32:53 +0700

    Update menu

--- Online_Star_Rail/menu.cpp
+++ Online_Star_Rail/menu.cpp
@@ -15,12 +15,19 @@ void displayCharacters() {
 	cout << "Here are your characters!" << endl;
 }
 
+void displayWish() {
+	cout << "Wish" << endl;
+}
+
 void displayMenu() {
 	int x;
-	cout << "1. Enter Shop" << endl;
-	cout << "2. Do Daily" << endl;
-	cout << "3. Open Characters" << endl;
-	cout << "4. Exit Game" << endl;
+	cout << "1. Shop" << endl;
+	cout << "2. Daily" << endl;
+	cout << "3. Characters" << endl;
+	cout << "4. Wish" << endl;
+	cout << "5. Bag" << endl;
+	cout << "6. Party" << endl;
+	cout << "7. Exit" << endl;
 	cin >> x;
 	//loging can be added here to track user choices
 	switch (x) {
@@ -34,6 +41,15 @@ void displayMenu() {
 		displayCharacters();
 		break;
 	case 4:
+		cout << "Wishing feature coming soon!" << endl;
+		break;
+	case 5:
+		cout << "Bag feature coming soon!" << endl;
+		break;
+	case 6:
+		cout << "Party feature coming soon!" << endl;
+		break;
+	case 7:
 		exit(0);
 		break;
 	default:
